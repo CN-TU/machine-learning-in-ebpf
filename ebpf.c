318ef6ccc28fc7b2b6f2c11710a11e58b3b9909b
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2020-12-27 21:29:36 +0100

    bpf draft program works

---
+++ ebpf.c
@@ -0,0 +1,90 @@
+#include <uapi/linux/ptrace.h>
+#include <net/sock.h>
+#include <bcc/proto.h>
+
+#define IP_TCP 6
+#define ETH_HLEN 14
+
+/*eBPF program.
+  Filter IP and TCP packets, having payload not empty
+  and containing "HTTP", "GET", "POST" ... as first bytes of payload
+  if the program is loaded as PROG_TYPE_SOCKET_FILTER
+  and attached to a socket
+  return  0 -> DROP the packet
+  return -1 -> KEEP the packet and return it to user space (userspace can read it from the socket_fd )
+*/
+int filter(struct __sk_buff *skb) {
+
+	u8 *cursor = 0;
+
+	struct ethernet_t *ethernet = cursor_advance(cursor, sizeof(*ethernet));
+	//filter IP packets (ethernet type = 0x0800)
+	if (!(ethernet->type == 0x0800)) {
+		goto DROP;
+	}
+
+	struct ip_t *ip = cursor_advance(cursor, sizeof(*ip));
+	//filter TCP packets (ip next protocol = 0x06)
+	if (ip->nextp != IP_TCP) {
+		goto DROP;
+	}
+
+	u32  tcp_header_length = 0;
+	u32  ip_header_length = 0;
+	u32  payload_offset = 0;
+	u32  payload_length = 0;
+
+	//calculate ip header length
+	//value to multiply * 4
+	//e.g. ip->hlen = 5 ; IP Header Length = 5 x 4 byte = 20 byte
+	ip_header_length = ip->hlen << 2;    //SHL 2 -> *4 multiply
+
+	// //check ip header length against minimum
+	// if (ip_header_length < sizeof(*ip)) {
+	// 	goto DROP;
+	// }
+
+	//shift cursor forward for dynamic ip header size
+	void *_ = cursor_advance(cursor, (ip_header_length-sizeof(*ip)));
+
+	struct tcp_t *tcp = cursor_advance(cursor, sizeof(*tcp));
+
+	//calculate tcp header length
+	//value to multiply *4
+	//e.g. tcp->offset = 5 ; TCP Header Length = 5 x 4 byte = 20 byte
+	tcp_header_length = tcp->offset << 2; //SHL 2 -> *4 multiply
+
+	//calculate payload offset and length
+	payload_offset = ETH_HLEN + ip_header_length + tcp_header_length;
+	payload_length = ip->tlen - ip_header_length - tcp_header_length;
+
+	//http://stackoverflow.com/questions/25047905/http-request-minimum-size-in-bytes
+	//minimum length of http request is always geater than 7 bytes
+	//avoid invalid access memory
+	//include empty payload
+	// if(payload_length < 7) {
+	// 	goto DROP;
+	// }
+
+	//load first 7 byte of payload into p (payload_array)
+	//direct access to skb not allowed
+	unsigned long p[7];
+	int i = 0;
+	for (i = 0; i < 7; i++) {
+		p[i] = load_byte(skb , payload_offset + i);
+	}
+
+	// //no HTTP match
+	// goto DROP;
+
+	bpf_trace_printk("Got packet with length: %u\n", ip->tlen);
+
+	//keep the packet and send it to userspace returning -1
+	KEEP:
+	return -1;
+
+	//drop the packet returning 0
+	DROP:
+	return 0;
+
+}
---
+++ test.py
@@ -0,0 +1,231 @@
+"""Example file for testing
+
+This creates a small testnet with ipaddresses from 192.168.0.0/24,
+one switch, and three hosts.
+"""
+
+import sys, os
+import io
+import time
+import math
+import signal
+import numpy as np
+import fnmatch
+sys.path.insert(0, os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+import subprocess
+import virtnet
+import statistics
+import argparse
+
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--bytes_to_capture', type=int, default=100)
+parser.add_argument('--delay', type=int, default=100)
+parser.add_argument('--rate', type=float, default=8)
+parser.add_argument('--time', type=float, default=10)
+parser.add_argument('--qdisc', type=str, default="fq")
+parser.add_argument('--cport', type=int, default=9000)
+parser.add_argument('--buffer_size', type=int, default=10)
+parser.add_argument('--run_scenario', type=str, default="")
+parser.add_argument('--store_pcaps', action='store_true')
+
+opt = parser.parse_args()
+print(opt)
+
+def run_commands(cmds, Popen=False):
+	if type(cmds) is not list:
+		cmds = [cmds]
+	return_stuff = []
+	for cmd in cmds:
+		if type(cmd) is tuple:
+			cmd, kwargs = cmd
+		else:
+			kwargs = {}
+		try:
+			print("cmd", cmd)#, "kwargs", kwargs)
+			if not Popen:
+				output = subprocess.run(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True, **kwargs)
+				# print("output", output)
+				return_stuff.append(output)
+			else:
+				popen = subprocess.Popen(cmd.split(" "), stdout=subprocess.PIPE, stderr=subprocess.STDOUT, **kwargs)
+				return_stuff.append(popen)
+		except subprocess.CalledProcessError as e:
+			print(e.cmd, e.returncode, e.output)
+			raise e
+	return return_stuff
+
+# print("os.environ", os.environ)
+
+def execute_popen_and_show_result(command, host=None):
+	parent = host if host is not None else subprocess
+	print(f"Executing{f' on host {host.name}' if host else ''}", command)
+	with parent.Popen(command.split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as cmd:
+		out, err = cmd.stdout.read(), cmd.stderr.read()
+		if out:
+			print("out", out.decode("utf-8"))
+		if err:
+			print("err", err.decode("utf-8"))
+
+number_of_seconds_the_competing_flow_starts_earlier = 5
+
+def run(vnet, prefix=""):
+
+		start_time = int(time.time() * 1000)
+		"Main functionality"
+
+		print("Building network...")
+		network = vnet.Network("192.168.0.0/24")
+		switch = vnet.Switch("sw")
+		hosts = []
+		for i in range(2):
+			host = vnet.Host("host{}".format(i))
+			host.connect(vnet.VirtualLink, switch, "eth0")
+			host["eth0"].add_ip(network)
+			execute_popen_and_show_result("ethtool -K eth0 gro off", host)
+			execute_popen_and_show_result("ethtool -K eth0 gso off", host)
+			execute_popen_and_show_result("ethtool -K eth0 tso off", host)
+			hosts.append(host)
+
+		for interface in switch.interfaces:
+			print("interface", interface)
+			execute_popen_and_show_result(f"ethtool -K {interface} gro off")
+			execute_popen_and_show_result(f"ethtool -K {interface} gso off")
+			execute_popen_and_show_result(f"ethtool -K {interface} tso off")
+
+			run_commands([f"tc qdisc add dev {interface} root handle 1: netem{f' delay {int(round(opt.delay/2))}ms'}", f"tc qdisc add dev {interface} parent 1: handle 2: htb default 21", f"tc class add dev {interface} parent 2: classid 2:21 htb rate {opt.rate if interface=='host10' else 100}mbit", f"tc qdisc add dev {interface} parent 2:21 handle 3: {opt.qdisc if interface=='host10' else 'fq'}{f' flow_limit {int(math.ceil(opt.buffer_size))}' if (interface=='host10' and opt.qdisc=='fq') else ''}{f' limit {int(math.ceil(opt.buffer_size))}' if (interface=='host10' and opt.qdisc=='pfifo') else ''}"])
+		vnet.update_hosts()
+
+		for i in range(len(hosts)):
+			with hosts[i].Popen("tc qdisc show dev eth0".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as qdisc_info:
+				qdisc_info_output = qdisc_info.stdout.read().decode("utf-8").split("\n")
+				print(f"qdisc_info_output host {i}", qdisc_info_output)
+
+		with hosts[0].Popen("ping -c 100 -i 0 host1".split(), stdout=subprocess.PIPE, stderr=subprocess.PIPE) as ping:
+			ping_output = ping.stdout.read().decode("utf-8").split("\n")
+			ping_output = [float(item.split()[-2][5:]) for item in ping_output if "time=" in item]
+			mean_rtt = statistics.mean(ping_output)
+			print("mean rtt", mean_rtt)
+			assert mean_rtt >= opt.delay, f"mean_rtt: {mean_rtt}, opt.delay: {opt.delay}"
+
+		server_popen = hosts[1].Popen("iperf3 -V -4 -s".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		ebpf_popen = hosts[1].Popen("python3 ebpf.py".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+		os.environ["file_name_for_logging"] = f"pcaps/{opt.qdisc}_{opt.delay}_{opt.rate}_{opt.time}_{start_time}.txt"
+		if opt.store_pcaps:
+			os.makedirs("pcaps", exist_ok=True)
+			tcpdump_sender_popens = []
+			tcpdump_receiver_popens = []
+
+			tcpdump_sender_popens.append(hosts[0].Popen(f"/usr/sbin/tcpdump -s {opt.bytes_to_capture} -i eth0 -w pcaps/sender_{prefix}_tcp_port{opt.cport+10}_{opt.qdisc}_{opt.delay}_{opt.rate}_{opt.time}_{start_time}.pcap tcp and dst port {opt.cport} or src port {opt.cport}".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+			tcpdump_receiver_popens.append(hosts[1].Popen(f"/usr/sbin/tcpdump -s {opt.bytes_to_capture} -i eth0 -w pcaps/receiver_{prefix}_tcp_port{opt.cport+10}_{opt.qdisc}_{opt.delay}_{opt.rate}_{opt.time}_{start_time}.pcap tcp and dst port {opt.cport} or src port {opt.cport}".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE))
+
+		client_popen = hosts[0].Popen(f"iperf3 -V -4 -t {opt.time} --cport {opt.cport} -c host1".split(" "), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		time.sleep(number_of_seconds_the_competing_flow_starts_earlier)
+
+		time.sleep(opt.time)
+
+		print("returncode before", client_popen.returncode)
+		client_popen.terminate()
+		print("returncode after", client_popen.returncode)
+		# import pdb; pdb.set_trace()
+		out, err = client_popen.stdout.read(), client_popen.stderr.read()
+		if out:
+			print("client out", out.decode("utf-8"))
+		if err:
+			print("client err", err.decode("utf-8"))
+		client_out = out
+
+		ebpf_popen.terminate()
+		out, err = ebpf_popen.stdout.read(), ebpf_popen.stderr.read()
+		if out:
+			print("ebpf out", out.decode("utf-8"))
+		if err:
+			print("ebpf err", err.decode("utf-8"))
+
+		server_popen.terminate()
+		out, err = server_popen.stdout.read(), server_popen.stderr.read()
+		if out:
+			print("server out", out.decode("utf-8"))
+		if err:
+			print("server err", err.decode("utf-8"))
+
+		if opt.store_pcaps:
+			for tcpdump_sender_popen in tcpdump_sender_popens:
+				tcpdump_sender_popen.terminate()
+				out, err = tcpdump_sender_popen.stdout.read(), tcpdump_sender_popen.stderr.read()
+				if out:
+					print("tcpdump out", out.decode("utf-8"))
+				if err:
+					print("tcpdump err", err.decode("utf-8"))
+
+			for tcpdump_receiver_popen in tcpdump_receiver_popens:
+				tcpdump_receiver_popen.terminate()
+				out, err = tcpdump_receiver_popen.stdout.read(), tcpdump_receiver_popen.stderr.read()
+				if out:
+					print("tcpdump out", out.decode("utf-8"))
+				if err:
+					print("tcpdump err", err.decode("utf-8"))
+
+			subprocess.check_output("chmod -R o+rw pcaps".split())
+
+		return client_out.decode("utf-8"), start_time
+
+if opt.run_scenario == "":
+	with virtnet.Manager() as context:
+		run(context)
+elif opt.run_scenario == "accuracy":
+	import sklearn.metrics
+	results_dict = {}
+	for bw_index, bw in enumerate(np.linspace(5,50,opt.how_many_values_per_parameter)):
+		for delay_index, delay in enumerate(np.linspace(10,100,opt.how_many_values_per_parameter)):
+			for buffer_index, buffer in enumerate(np.linspace(1,100,opt.how_many_values_per_parameter)):
+				for fq_index, fq in enumerate([False, True]):
+					opt.rate = int(round(bw))
+					opt.delay = int(round(delay))
+					opt.buffer_size = int(round(buffer))
+					opt.qdisc = "fq" if fq else "pfifo"
+					opt.time = 10
+
+					with virtnet.Manager() as context:
+						client_output, timestamp = run(context, "accuracy")
+					assert client_output != ""
+					contained_vegas = "Starting Vegas" in client_output
+					contained_pcc = "Starting PCC Classic" in client_output
+
+					results_dict[(bw, delay, buffer, fq)] = (contained_vegas, contained_pcc)
+
+	invalids = []
+	false_predictions = []
+	predictions = []
+	for (bw, delay, buffer, fq), (is_vegas, is_pcc) in results_dict.items():
+		is_invalid = (not is_vegas and not is_pcc)
+		if is_invalid:
+			invalids.append(((bw, delay, buffer, fq), (is_vegas, is_pcc)))
+		if not is_invalid:
+			predictions.append((fq, is_vegas))
+			if fq != is_vegas:
+				false_predictions.append(((bw, delay, buffer, fq), is_vegas))
+
+	print("invalids", len(invalids), "total", len(results_dict))
+	print("invalids", invalids)
+	confusion_matrix_input = list(zip(*predictions))
+	accuracy_score = sklearn.metrics.accuracy_score(*confusion_matrix_input)
+	print("accuracy_score", accuracy_score)
+	confusion_matrix = sklearn.metrics.confusion_matrix(*confusion_matrix_input)
+	print("confusion_matrix", confusion_matrix)
+	print("false_predictions", false_predictions)
+
+if opt.run_scenario == "just_one_flow":
+	opt.time = 20
+	opt.store_pcaps = False
+	opt.buffer_size = 100
+	opt.rate = 10
+	opt.delay = 10
+
+	opt.qdisc = "fq"
+	with virtnet.Manager() as context:
+		client_output, timestamp = run(context, "just_one_flow")
