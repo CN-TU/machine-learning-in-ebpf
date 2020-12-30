2c7979d4157350de26a043236334164c1a1b0ec0
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2020-12-31 00:51:44 +0100

    reading of data

--- ebpf.c
+++ ebpf.c
@@ -7,115 +7,117 @@
 #define ETH_HLEN 14
 
 /*eBPF program.
-  Filter IP and TCP packets, having payload not empty
-  and containing "HTTP", "GET", "POST" ... as first bytes of payload
-  if the program is loaded as PROG_TYPE_SOCKET_FILTER
-  and attached to a socket
-  return  0 -> DROP the packet
-  return -1 -> KEEP the packet and return it to user space (userspace can read it from the socket_fd )
+	Filter IP and TCP packets, having payload not empty
+	and containing "HTTP", "GET", "POST" ... as first bytes of payload
+	if the program is loaded as PROG_TYPE_SOCKET_FILTER
+	and attached to a socket
+	return  0 -> DROP the packet
+	return -1 -> KEEP the packet and return it to user space (userspace can read it from the socket_fd )
 */
 int filter(struct __sk_buff *skb) {
 
 	u8 *cursor = 0;
-  // int current_state;
+	// int current_state;
 
-  /* Initialize most fields to 0 in case we do not parse associated headers.
-   * The alternative is to set it to 0 once we know we will not meet the header
-   * (e.g. when we see ARP, we won't have dst IP / port...). It would prevent
-   * to affect a value twice in some cases, but it is prone to error when
-   * adding parsing for other protocols.
-   */
-  // struct StateTableKey state_idx;
-  // // state_idx.ether_type // Will be set anyway
-  // state_idx.__padding16 = 0;
-  // state_idx.ip_src = 0;
-  // state_idx.ip_dst = 0;
+	/* Initialize most fields to 0 in case we do not parse associated headers.
+	 * The alternative is to set it to 0 once we know we will not meet the header
+	 * (e.g. when we see ARP, we won't have dst IP / port...). It would prevent
+	 * to affect a value twice in some cases, but it is prone to error when
+	 * adding parsing for other protocols.
+	 */
+	// struct StateTableKey state_idx;
+	// // state_idx.ether_type // Will be set anyway
+	// state_idx.__padding16 = 0;
+	// state_idx.ip_src = 0;
+	// state_idx.ip_dst = 0;
 
-  struct XFSMTableKey xfsm_idx;
-  // xfsm_idx.state // Will be set anyway before XFSM lookup
-  xfsm_idx.l4_proto = 0;
+	struct XFSMTableKey xfsm_idx;
+	// xfsm_idx.state // Will be set anyway before XFSM lookup
+	xfsm_idx.l4_proto = 0;
 	xfsm_idx.ip_src = 0;
 	xfsm_idx.ip_dst = 0;
-  xfsm_idx.src_port = 0;
-  xfsm_idx.dst_port = 0;
-  xfsm_idx.__padding8  = 0;
-  xfsm_idx.__padding16 = 0;
+	xfsm_idx.src_port = 0;
+	xfsm_idx.dst_port = 0;
+	xfsm_idx.__padding8  = 0;
+	xfsm_idx.__padding16 = 0;
 
-  struct ethernet_t *ethernet;
-  struct ip_t       *ip;
-  struct udp_t      *l4;
+	struct ethernet_t *ethernet;
+	struct ip_t       *ip;
+	struct udp_t      *l4;
 
-  /* Headers parsing */
+	/* Headers parsing */
 
-  ethernet: {
-    ethernet = cursor_advance(cursor, sizeof(*ethernet));
-    // state_idx.ether_type = ethernet->type;
+	ethernet: {
+		ethernet = cursor_advance(cursor, sizeof(*ethernet));
+		// state_idx.ether_type = ethernet->type;
 
-    switch (ethernet->type) {
-      case ETH_P_IP:   goto ip;
-      case ETH_P_ARP:  goto arp;
-      default:         goto EOP;
-    }
-  }
+		switch (ethernet->type) {
+			case ETH_P_IP:   goto ip;
+			case ETH_P_ARP:  goto arp;
+			default:         goto EOP;
+		}
+	}
 
-  ip: {
-    ip = cursor_advance(cursor, sizeof(*ip));
-    // state_idx.ip_src = ip->src;
-    // state_idx.ip_dst = ip->dst;
+	ip: {
+		ip = cursor_advance(cursor, sizeof(*ip));
+		// state_idx.ip_src = ip->src;
+		// state_idx.ip_dst = ip->dst;
 
-    xfsm_idx.ip_src = ip->src;
-    xfsm_idx.ip_dst = ip->dst;
+		xfsm_idx.ip_src = ip->src;
+		xfsm_idx.ip_dst = ip->dst;
 
-    switch (ip->nextp) {
-      case IPPROTO_TCP: goto l4;
-      case IPPROTO_UDP: goto l4;
+		switch (ip->nextp) {
+			case IPPROTO_TCP: goto l4;
+			case IPPROTO_UDP: goto l4;
 			// FIXME: Is this correct?
-      default:          goto l4;
-    }
-  }
+			default:          goto l4;
+		}
+	}
 
-  arp: {
-    /* We could parse ARP packet here if we needed to retrieve some fields from
-     * the ARP header for the lookup.
-     */
-    goto xfsmlookup;
-  }
+	arp: {
+		/* We could parse ARP packet here if we needed to retrieve some fields from
+		 * the ARP header for the lookup.
+		 */
+		goto xfsmlookup;
+	}
 
-  l4: {
-    /* Here We only need dst and src ports from L4, and they are at the same
-     * location for TCP and UDP; so do not switch on cases, just use UDP
-     * cursor.
-     */
-    l4 = cursor_advance(cursor, sizeof(*l4));
-    goto xfsmlookup;
-  }
+	l4: {
+		/* Here We only need dst and src ports from L4, and they are at the same
+		 * location for TCP and UDP; so do not switch on cases, just use UDP
+		 * cursor.
+		 */
+		l4 = cursor_advance(cursor, sizeof(*l4));
+		goto xfsmlookup;
+	}
 
-  /* Tables lookups */
+	/* Tables lookups */
 
-  // statelookup: {
-  //   struct StateTableLeaf *state_val = state_table.lookup(&state_idx);
+	// statelookup: {
+	//   struct StateTableLeaf *state_val = state_table.lookup(&state_idx);
 
-  //   if (state_val) {
-  //     current_state = state_val->state;
-  //     /* If we found a state, go on and search XFSM table for this state and
-  //      * for current event.
-  //      */
-  //     goto xfsmlookup;
-  //   }
-  //   goto EOP;
-  // }
+	//   if (state_val) {
+	//     current_state = state_val->state;
+	//     /* If we found a state, go on and search XFSM table for this state and
+	//      * for current event.
+	//      */
+	//     goto xfsmlookup;
+	//   }
+	//   goto EOP;
+	// }
 
-  xfsmlookup: {
-    /* We don't want to match on L4 src port, so set it at 0 here and in XFSM
-     * initialization, since we have no wildcard mechanism. */
-    // xfsm_idx.state    = current_state;
-    xfsm_idx.l4_proto = ip->nextp;
-    xfsm_idx.src_port = l4->sport;
-    xfsm_idx.dst_port = l4->dport;
-    xfsm_idx.__padding8  = 0;
-    xfsm_idx.__padding16 = 0;
+	xfsmlookup: {
+		/* We don't want to match on L4 src port, so set it at 0 here and in XFSM
+		 * initialization, since we have no wildcard mechanism. */
+		// xfsm_idx.state    = current_state;
+		xfsm_idx.l4_proto = ip->nextp;
+		xfsm_idx.src_port = l4->sport;
+		xfsm_idx.dst_port = l4->dport;
+		xfsm_idx.__padding8  = 0;
+		xfsm_idx.__padding16 = 0;
 
-    struct XFSMTableLeaf *xfsm_val = xfsm_table.lookup(&xfsm_idx);
+		// bpf_trace_printk("Received packet with length %u from port %u to port %u\n", ip->tlen, (u32) l4->sport, (u32) l4->dport);
+
+		struct XFSMTableLeaf *xfsm_val = xfsm_table.lookup(&xfsm_idx);
 
 		if (!xfsm_val) {
 			struct XFSMTableLeaf zero = {0, {0,0,0,0,0,0,0,0,0,0,0,0}, false};
@@ -123,43 +125,43 @@ int filter(struct __sk_buff *skb) {
 			xfsm_val = xfsm_table.lookup(&xfsm_idx);
 		}
 
-    // if (xfsm_val) {
-      /* Update state table. We re-use the StateTableKey we had initialized
-       * already. We update this rule with the new state provided by XFSM
-       * table.
-       */
-      // struct StateTableLeaf new_state = { xfsm_val->next_state };
-      // state_table.update(&state_idx, &new_state);
+		// if (xfsm_val) {
+			/* Update state table. We re-use the StateTableKey we had initialized
+			 * already. We update this rule with the new state provided by XFSM
+			 * table.
+			 */
+			// struct StateTableLeaf new_state = { xfsm_val->next_state };
+			// state_table.update(&state_idx, &new_state);
 
-      /* At last, execute the action for the current state, that we obtained
-       * from the XFSM table.
-       * Users should add new actions here.
-       */
-      // switch (xfsm_val->action) {
-      //   case ACTION_DROP:
-      //     return TC_CLS_DEFAULT;
-      //   case ACTION_FORWARD:
-      //     return TC_CLS_NOMATCH;
-      //   default:
-      //     return TC_CLS_NOMATCH; // XXX Should actually return an error code.
-      // }
-    // }
+			/* At last, execute the action for the current state, that we obtained
+			 * from the XFSM table.
+			 * Users should add new actions here.
+			 */
+			// switch (xfsm_val->action) {
+			//   case ACTION_DROP:
+			//     return TC_CLS_DEFAULT;
+			//   case ACTION_FORWARD:
+			//     return TC_CLS_NOMATCH;
+			//   default:
+			//     return TC_CLS_NOMATCH; // XXX Should actually return an error code.
+			// }
+		// }
 
-    /* So we did not find a match in XFSM table... For port knocking, default
-     * action is "return to initial state". We have yet to find a way to
-     * properly implement a default action. XXX
-     */
-    // enum states {
-    //   DEFAULT,
-    //   STEP_1,
-    //   STEP_2,
-    //   OPEN
-    // };
-    // struct StateTableLeaf new_state = { DEFAULT };
-    // state_table.update(&state_idx, &new_state);
-    return TC_CLS_NOMATCH;
-  }
+		/* So we did not find a match in XFSM table... For port knocking, default
+		 * action is "return to initial state". We have yet to find a way to
+		 * properly implement a default action. XXX
+		 */
+		// enum states {
+		//   DEFAULT,
+		//   STEP_1,
+		//   STEP_2,
+		//   OPEN
+		// };
+		// struct StateTableLeaf new_state = { DEFAULT };
+		// state_table.update(&state_idx, &new_state);
+		return TC_CLS_NOMATCH;
+	}
 
 EOP:
-  return TC_CLS_NOMATCH;
+	return TC_CLS_NOMATCH;
 }
--- ebpf.py
+++ ebpf.py
@@ -2,6 +2,7 @@ import bcc
 import socket
 import os
 import numpy as np
+import ctypes as ct
 
 interface="eth0"
 
@@ -10,26 +11,49 @@ print(f"binding socket to #{interface}")
 prefix_path = "/home/max/repos/adversarial-recurrent-ids/runs/Dec29_19-15-36_hyperion_0_3"
 
 with open('%s_childrenLeft' % prefix_path, 'rb') as f:
-	children_left = np.fromfile(f, dtype=np.uint64)
-	bcc.BPF(text=f'BPF_ARRAY(children_left, u64, {len(children_left)});')
-	children_left_table = b.get_table("children_left")
-	for i in range(len(children_left)):
-		children_left_table[i] = children_left[i]
+	children_left = np.fromfile(f, dtype=np.int64)
 with open('%s_childrenRight' % prefix_path, 'rb') as f:
-	children_right = np.fromfile(f, dtype=np.uint64)
-	bcc.BPF(text=f'BPF_ARRAY(children_right, u64, {len(children_right)});')
+	children_right = np.fromfile(f, dtype=np.int64)
 with open('%s_value' % prefix_path, 'rb') as f:
-	value = np.fromfile(f, dtype=np.uint64)
-	bcc.BPF(text=f'BPF_ARRAY(value, u64, {len(value)});')
+	value = np.fromfile(f, dtype=np.int64)
 with open('%s_feature' % prefix_path, 'rb') as f:
-	feature = np.fromfile(f, dtype=np.uint64)
-	bcc.BPF(text=f'BPF_ARRAY(feature, u64, {len(feature)});')
+	feature = np.fromfile(f, dtype=np.int64)
 with open('%s_threshold' % prefix_path, 'rb') as f:
 	threshold = np.fromfile(f, dtype=np.int64)
-	bcc.BPF(text=f'BPF_ARRAY(threshold, s64, {len(threshold)});')
+
+maps_string = f"""
+BPF_ARRAY(children_left, s64, {len(children_left)});
+BPF_ARRAY(children_right, s64, {len(children_right)});
+BPF_ARRAY(value, s64, {len(value)});
+BPF_ARRAY(feature, s64, {len(feature)});
+BPF_ARRAY(threshold, s64, {len(threshold)});
+"""
+
+with open("ebpf.c", "r") as f:
+	actual_bpf_text = f.read()
+
+ebpf_program = maps_string + actual_bpf_text
 
 # initialize BPF - load source code from http-parse-simple.c
-bpf = bcc.BPF(src_file="ebpf.c",debug = 0)
+bpf = bcc.BPF(text=ebpf_program)
+
+children_left_table = bpf.get_table("children_left")
+for i in range(len(children_left)):
+	children_left_table[i] = ct.c_long(children_left[i].item())
+children_right_table = bpf.get_table("children_right")
+for i in range(len(children_right)):
+	children_right_table[i] = ct.c_long(children_right[i].item())
+value_table = bpf.get_table("value")
+for i in range(len(value)):
+	value_table[i] = ct.c_long(value[i].item())
+threshold_table = bpf.get_table("threshold")
+for i in range(len(threshold)):
+	threshold_table[i] = ct.c_long(threshold[i].item())
+feature_table = bpf.get_table("feature")
+for i in range(len(feature)):
+	feature_table[i] = ct.c_long(feature[i].item())
+
+
 
 #load eBPF program http_filter of type SOCKET_FILTER into the kernel eBPF vm
 #more info about eBPF program types
