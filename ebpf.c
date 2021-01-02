44c2769adf237ca8d2cb2b5a221b3ddf20f1f2cb
Maximilian Bachl <maximilian.bachl@nt.tuwien.ac.at>
2021-01-02 02:22:09 +0100

    Implement flow storage and features

--- ebpf.c
+++ ebpf.c
@@ -1,10 +1,18 @@
-#include <uapi/linux/ptrace.h>
+// #include <uapi/linux/ptrace.h>
 #include <net/sock.h>
 #include <bcc/proto.h>
 #include "openstate.h"
 
+// #define abs(x) ((x)<0 ? -(x) : (x))
+
+#define MAX_TREE_DEPTH 100
+
 #define IP_TCP 6
 #define ETH_HLEN 14
+
+#define FIXED_POINT_DIGITS 16
+
+BPF_ARRAY(num_processed, u64, 1);
 
 /*eBPF program.
 	Filter IP and TCP packets, having payload not empty
@@ -15,6 +23,14 @@
 	return -1 -> KEEP the packet and return it to user space (userspace can read it from the socket_fd )
 */
 int filter(struct __sk_buff *skb) {
+
+	u64 ts = bpf_ktime_get_ns();
+	int zero = 0;
+	u64* current_value = num_processed.lookup(&zero);
+	if (current_value != NULL) {
+		(*current_value) += 1;
+		num_processed.update(&zero, current_value);
+	}
 
 	u8 *cursor = 0;
 	// int current_state;
@@ -112,6 +128,10 @@ int filter(struct __sk_buff *skb) {
 		xfsm_idx.l4_proto = ip->nextp;
 		xfsm_idx.src_port = l4->sport;
 		xfsm_idx.dst_port = l4->dport;
+		if (l4->sport > l4->dport) {
+			xfsm_idx.src_port = l4->dport;
+			xfsm_idx.dst_port = l4->sport;
+		}
 		xfsm_idx.__padding8  = 0;
 		xfsm_idx.__padding16 = 0;
 
@@ -120,10 +140,65 @@ int filter(struct __sk_buff *skb) {
 		struct XFSMTableLeaf *xfsm_val = xfsm_table.lookup(&xfsm_idx);
 
 		if (!xfsm_val) {
-			struct XFSMTableLeaf zero = {0, {0,0,0,0,0,0,0,0,0,0,0,0}, false};
+			struct XFSMTableLeaf zero = {0, 0, {0,0,0,0,0,0}, 0, 0, false};
+			// XXX: WTF is this necessary?
+			zero.actual_src_port = l4->sport;
+			zero.actual_dst_port = l4->dport;
+			// struct XFSMTableLeaf zero = {0, 0, {0,0,0,0,0,0}, false};
 			xfsm_table.insert(&xfsm_idx, &zero);
 			xfsm_val = xfsm_table.lookup(&xfsm_idx);
 		}
+
+		if (xfsm_val != NULL) {
+			xfsm_val->num_packets += 1;
+
+			s64 sport = xfsm_val->actual_src_port;
+			s64 dport = xfsm_val->actual_dst_port;
+			s64 protocol_identifier = ip->nextp;
+			s64 total_length = ip->tlen;
+
+			s64 delta = 0;
+			if (xfsm_val->last_packet_timestamp > 0) {
+				delta = ts - xfsm_val->last_packet_timestamp;
+			}
+			xfsm_val->last_packet_timestamp = ts;
+
+			s64 direction = l4->sport == xfsm_val->actual_src_port;
+
+			sport <<= FIXED_POINT_DIGITS;
+			dport <<= FIXED_POINT_DIGITS;
+			protocol_identifier <<= FIXED_POINT_DIGITS;
+			total_length <<= FIXED_POINT_DIGITS;
+			delta <<= FIXED_POINT_DIGITS;
+			direction <<= FIXED_POINT_DIGITS;
+
+			xfsm_val->features[0] += total_length;
+			xfsm_val->features[1] += delta;
+			xfsm_val->features[2] += direction;
+
+			s64 avg_total_length = xfsm_val->features[0]/xfsm_val->num_packets;
+			s64 avg_delta = xfsm_val->features[1]/xfsm_val->num_packets;
+			s64 avg_direction = xfsm_val->features[2]/xfsm_val->num_packets;
+
+			xfsm_val->features[3] += abs(total_length-avg_total_length);
+			xfsm_val->features[4] += abs(delta-avg_delta);
+			xfsm_val->features[5] += abs(direction-avg_direction);
+
+			s64 avg_dev_total_length = xfsm_val->features[3]/xfsm_val->num_packets;
+			s64 avg_dev_delta = xfsm_val->features[4]/xfsm_val->num_packets;
+			s64 avg_dev_direction = xfsm_val->features[5]/xfsm_val->num_packets;
+
+			s64 all_features[12] = {sport, dport, protocol_identifier, total_length, delta, direction, avg_total_length, avg_delta, avg_direction, avg_dev_total_length, avg_dev_delta, avg_dev_direction};
+		}
+
+		// node = self.nodes
+		// # While node not a leaf
+		// while node.left_child != _TREE_LEAF:
+		// 		# ... and node.right_child != _TREE_LEAF:
+		// 		if X_ndarray[i, node.feature] <= node.threshold:
+		// 				node = &self.nodes[node.left_child]
+		// 		else:
+		// 				node = &self.nodes[node.right_child]
 
 		// if (xfsm_val) {
 			/* Update state table. We re-use the StateTableKey we had initialized
--- ebpf.py
+++ ebpf.py
@@ -3,9 +3,16 @@ import socket
 import os
 import numpy as np
 import ctypes as ct
+import atexit
+import signal
+import time
+import sys
+
+time_to_run = float(sys.argv[1])
+
+starttime = time.time()
 
 interface="eth0"
-
 print(f"binding socket to #{interface}")
 
 prefix_path = "/home/max/repos/adversarial-recurrent-ids/runs/Dec29_19-15-36_hyperion_0_3"
@@ -29,7 +36,7 @@ BPF_ARRAY(feature, s64, {len(feature)});
 BPF_ARRAY(threshold, s64, {len(threshold)});
 """
 
-with open("ebpf.c", "r") as f:
+with open("ebpf.c") as f:
 	actual_bpf_text = f.read()
 
 ebpf_program = maps_string + actual_bpf_text
@@ -53,7 +60,12 @@ feature_table = bpf.get_table("feature")
 for i in range(len(feature)):
 	feature_table[i] = ct.c_long(feature[i].item())
 
+bpf.get_table("num_processed")[0] = ct.c_ulong(0)
 
+def show_counter():
+	ran_for = time.time() - starttime
+	print(f"Ran for {ran_for}s. Processed: {bpf.get_table('num_processed')[0].value}")
+atexit.register(show_counter)
 
 #load eBPF program http_filter of type SOCKET_FILTER into the kernel eBPF vm
 #more info about eBPF program types
@@ -64,83 +76,21 @@ function_filter = bpf.load_func("filter", bcc.BPF.SOCKET_FILTER)
 #attach bpf program to socket created
 bcc.BPF.attach_raw_socket(function_filter, interface)
 
-#get file descriptor of the socket previously created inside BPF.attach_raw_socket
-socket_fd = function_filter.sock
+# #get file descriptor of the socket previously created inside BPF.attach_raw_socket
+# socket_fd = function_filter.sock
 
-#create python socket object, from the file descriptor
-sock = socket.fromfd(socket_fd,socket.PF_PACKET,socket.SOCK_RAW,socket.IPPROTO_IP)
-#set it as blocking socket
-sock.setblocking(True)
+# #create python socket object, from the file descriptor
+# sock = socket.fromfd(socket_fd,socket.PF_PACKET,socket.SOCK_RAW,socket.IPPROTO_IP)
+# #set it as blocking socket
+# sock.setblocking(False)
 
-while True:
-	#retrieve raw packet from socket
-	packet_str = os.read(socket_fd,2048)
+# signal.pause()
+time.sleep(time_to_run)
 
-	#DEBUG - print raw packet in hex format
-	#packet_hex = toHex(packet_str)
-	#print ("%s" % packet_hex)
+# i = 0
+# while True:
+# 	#retrieve raw packet from socket
+# 	packet_str = os.read(socket_fd,2048)
 
-	#convert packet into bytearray
-	packet_bytearray = bytearray(packet_str)
-
-	#ethernet header length
-	ETH_HLEN = 14
-
-	#IP HEADER
-	#https://tools.ietf.org/html/rfc791
-	# 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-	# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	# |Version|  IHL  |Type of Service|          Total Length         |
-	# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	#
-	#IHL : Internet Header Length is the length of the internet header
-	#value to multiply * 4 byte
-	#e.g. IHL = 5 ; IP Header Length = 5 * 4 byte = 20 byte
-	#
-	#Total length: This 16-bit field defines the entire packet size,
-	#including header and data, in bytes.
-
-	#calculate packet total length
-	total_length = packet_bytearray[ETH_HLEN + 2]               #load MSB
-	total_length = total_length << 8                            #shift MSB
-	total_length = total_length + packet_bytearray[ETH_HLEN+3]  #add LSB
-
-	#calculate ip header length
-	ip_header_length = packet_bytearray[ETH_HLEN]               #load Byte
-	ip_header_length = ip_header_length & 0x0F                  #mask bits 0..3
-	ip_header_length = ip_header_length << 2                    #shift to obtain length
-
-	#TCP HEADER
-	#https://www.rfc-editor.org/rfc/rfc793.txt
-	#  12              13              14              15
-	#  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
-	# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	# |  Data |           |U|A|P|R|S|F|                               |
-	# | Offset| Reserved  |R|C|S|S|Y|I|            Window             |
-	# |       |           |G|K|H|T|N|N|                               |
-	# +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-	#
-	#Data Offset: This indicates where the data begins.
-	#The TCP header is an integral number of 32 bits long.
-	#value to multiply * 4 byte
-	#e.g. DataOffset = 5 ; TCP Header Length = 5 * 4 byte = 20 byte
-
-	#calculate tcp header length
-	tcp_header_length = packet_bytearray[ETH_HLEN + ip_header_length + 12]  #load Byte
-	tcp_header_length = tcp_header_length & 0xF0                            #mask bit 4..7
-	tcp_header_length = tcp_header_length >> 2                              #SHR 4 ; SHL 2 -> SHR 2
-
-	#calculate payload offset
-	payload_offset = ETH_HLEN + ip_header_length + tcp_header_length
-
-	#print first line of the HTTP GET/POST request
-	#line ends with 0xOD 0xOA (\r\n)
-	#(if we want to print all the header print until \r\n\r\n)
-	# for i in range (payload_offset,len(packet_bytearray)-1):
-	#   if (packet_bytearray[i]== 0x0A):
-	#     if (packet_bytearray[i-1] == 0x0D):
-	#       break
-	#   print ("%c" % chr(packet_bytearray[i]), end = "")
-	# print("")
-
-	# print("Got packet in python", total_length)
+# 	if i%1000 == 0:
+# 		print("spam")
